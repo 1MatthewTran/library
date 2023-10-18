b8821a9d1a79b827e7867334c43ec7a56d5e1a08
1MatthewTran <1matthewtran@gmail.com>
2023-10-18 18:05:22 -0400

    add css/html layout

---
+++ library.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function createBook(author, title, numPages, isRead){
+    return { author, title, numPages, isRead };
+}
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+
+}
+
+function displayBooks(){
+}
